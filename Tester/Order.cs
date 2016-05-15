16ef3f0080edc4c54a01f8cfb76b32999458ecac
pdo9141 <pdo9141@gmail.com>
2016-05-15 00:40:45 -0700

    Add project files.

---
+++ Tester/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    public class Order : ContractBase
+    {
+        public int ID { get; set; }
+
+        public int ClientID { get; set; }
+
+        public string ReferenceNumber { get; set; }
+    }
+}
